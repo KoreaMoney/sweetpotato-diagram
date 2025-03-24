c70337d57fa41b93f42775d6607a2b568fcf40b3
KoreaMoney <kimdowon1404@gmail.com>
2025-03-24 09:09:47 +0900

    first commit

---
+++ src/Diagram.jsx
@@ -0,0 +1,5 @@
+const Diagram = () => {
+  return <div className="bg-blue-500 font-bold text-4xl text-white">Diagram</div>;
+};
+
+export default Diagram;
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Diagram from "./Diagram.jsx";
+
+createRoot(document.getElementById("root")).render(<Diagram />);
